4e04ee2794b0706784e95d2e1f127a231e3586ca
João Vitor de Souza <jooao_souza@hotmail.com>
2019-10-03 20:18:57 -0300

    Adicionado tabela estatica

--- src/pages/Relatorio/index.js
+++ src/pages/Relatorio/index.js
@@ -1,31 +1,61 @@
 import React from "react";
-import { Card, Col, Row } from "antd";
+import { Table, Divider, Icon, Card, Button } from "antd";
+import { Link } from "react-router-dom";
 
 import "./index.scss";
+
+const columns = [
+  {
+    title: "Descrição",
+    dataIndex: "descricao",
+    key: "descricao"
+  },
+  {
+    title: "Unidade",
+    dataIndex: "unidade",
+    key: "unidade"
+  },
+  {
+    title: "Quantidade",
+    dataIndex: "quantidade",
+    key: "quantidade"
+  },
+  {
+    title: "Ações",
+    key: "acao",
+    render: (text, record) => (
+      <span>
+        <Icon type="edit" theme="filled" />
+        <Divider type="vertical" />
+        <Icon type="delete" theme="filled" />
+      </span>
+    )
+  }
+];
+
+const data = [
+  {
+    key: "1",
+    descricao: "Batata",
+    unidade: "Quilograma",
+    quantidade: "10"
+  }
+];
 
 export default function CadastroItem() {
   return (
     <>
-      <Row gutter={16} style={{ width: "100%" }}>
-        <Col span={12}>
-          <Card
-            title="Card title"
-            bordered={true}
-            style={{ width: "100%", borderRadius: "24px", padding: "50px" }}
-          >
-            Card content
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card
-            title="Card title"
-            bordered={false}
-            style={{ width: "100%", borderRadius: "24px", padding: "50px" }}
-          >
-            Card content
-          </Card>
-        </Col>
-      </Row>
+      <Card
+        title="Itens cadastrados"
+        style={{ width: "100%", borderRadius: "24px", padding: "50px" }}
+      >
+        <Table columns={columns} dataSource={data} />
+        <Link to="/">
+          <Button type="primary" htmlType="submit">
+            Adicionar novo item
+          </Button>
+        </Link>
+      </Card>
     </>
   );
 }
